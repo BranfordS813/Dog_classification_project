a065f3a0ba47e9b4760ed2658b762e20340abbb4
BranfordS813 <sanfordbrandon22@yahoo.com>
2025-11-30 21:22:14 -0500

    Pick Image button works with permissions

--- Dog_Breed_Prediction/ViewController.swift
+++ Dog_Breed_Prediction/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Debug: Check if outlets are connected
+        print("viewDidLoad called")
+        print("imageView is connected: \(imageView != nil)")
+        print("predictionText is connected: \(predictionText != nil)")
+        print("btn is connected: \(btn != nil)")
     }
 
     
@@ -22,14 +28,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBAction func btnClick(_ sender: Any) {
         
+        print("Button clicked!")
+        
+        // Confirm delegate method for picking an image
+        
+        // Check if photo library is available
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("ERROR: Photo library is not available")
+            return
+        }
+        
+        print("Photo library is available, creating image picker...")
+        
         let imagePicker = UIImagePickerController()
         
         imagePicker.delegate = self
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .photoLibrary
         
-        present(imagePicker, animated: true, completion: nil)
+        print("About to present image picker...")
+        
+        // Make sure we're on the main thread
+        DispatchQueue.main.async {
+            self.present(imagePicker, animated: true, completion: {
+                print("Image picker presented successfully")
+            })
+        }
     }
+    
+    // Impliment two delegate methods for the image picker: imagePickerController and imagePickerControllerDidCancel --> called when user selects imaage from library or cancles selecting image from library
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Safely unwrap the image instead of force unwrapping
+        if let pickedImage = info[.originalImage] as? UIImage {
+            imageView.image = pickedImage
+            
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     
 }
 
