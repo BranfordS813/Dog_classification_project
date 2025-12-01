eaea9fe861540f2dec61402de72829b1a784fd75
BranfordS813 <sanfordbrandon22@yahoo.com>
2025-11-30 20:17:24 -0500

    setting up image picker button

--- Dog_Breed_Prediction/ViewController.swift
+++ Dog_Breed_Prediction/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBOutlet weak var predictionText: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var btn: UIButton!
+    
+    
+    @IBAction func btnClick(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
